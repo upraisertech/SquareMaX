3d87fc49c565d5179e3f2fa767ae6d6384a95813
upraisertech <akoredesalaudeen54@gmail.com>
2023-12-18 20:19:40 +0100

    first commit

--- src/_root/pages/Calculator/CalcTap.tsx
+++ src/_root/pages/Calculator/CalcTap.tsx
@@ -1,12 +1,9 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import Forex from "./Forex/index";
 import Crypto from "./Crypto/index";
 import Percentage from "./Percentage/index";
-import myContext from "@/context/data/myContext";
 
 const SettingsHeader = () => {
-  const context = useContext(myContext);
-  const { mode } = context;
   const [activeTab, setActiveTab] = useState(1);
 
   const handleTabClick = (tabNumber: React.SetStateAction<number>) => {
@@ -15,30 +12,17 @@ const SettingsHeader = () => {
 
   return (
     <div className="w-full mt-[-6em]">
-      <div
-        className="flex border-b gap-5 mb-2 mx-auto items-center justify-between"
-        style={{ color: mode === "dark" ? "white" : "" }}
-      >
+      <div className="flex border-b gap-5 mb-2 mx-auto items-center justify-between">
         <button
           className={`py-2 text-[17px] h-[40px] focus:outline-none w-full
-          ${
-            mode === "dark"
-              ? "border-primary-A1 text-primary-A2"
-              : "border-primary-A2 text-primary-A1"
-          } 
           ${
             activeTab === 1
               ? "border-primary-A1 border-t-none border-b-[4px] "
               : ""
           }`}
-          onClick={() => handleTabClick(1)}
-        >
+          onClick={() => handleTabClick(1)}>
           <div className="flex flex-col gap-0.5 items-center justify-center px-3">
-            <div
-              className="text-[17px] w-auto"
-            >
-              Percentage
-            </div>
+            <div className="text-[17px] w-auto">Percentage</div>
           </div>
         </button>
 
@@ -48,16 +32,10 @@ const SettingsHeader = () => {
             role="button"
             className={`py-2 text-[17px] h-[40px] focus:outline-none
             ${
-              mode === "dark"
-                ? "border-primary-A1 text-primary-A2"
-                : "border-primary-A2 text-primary-A1"
-            } 
-            ${
               activeTab === 2 || activeTab === 3
                 ? "border-t-none border-b-[4px]"
                 : ""
-            }`}
-          >
+            }`}>
             PositionSize
           </div>
           <ul className="shadow menu dropdown-content z-[1] gap-3 bg-white rounded-box w-32">
@@ -65,14 +43,9 @@ const SettingsHeader = () => {
               className={`py-2 text-[17px] h-[40px] focus:outline-none ${
                 activeTab === 2 ? "text-primary-A2" : "text-black"
               }`}
-              onClick={() => handleTabClick(2)}
-            >
+              onClick={() => handleTabClick(2)}>
               <div className="flex flex-col gap-0.5 items-center justify-center px-3">
-                <div
-                  className="text-[17px] w-auto"
-                >
-                  Forex
-                </div>
+                <div className="text-[17px] w-auto">Forex</div>
               </div>
             </button>
 
@@ -80,14 +53,9 @@ const SettingsHeader = () => {
               className={`py-2 text-[17px] h-[40px] focus:outline-none ${
                 activeTab === 3 ? "text-primary-A2" : "text-black"
               }`}
-              onClick={() => handleTabClick(3)}
-            >
+              onClick={() => handleTabClick(3)}>
               <div className="flex flex-col gap-0.5 items-center justify-center px-3">
-                <div
-                  className="text-[17px] w-auto"
-                >
-                  Crypto
-                </div>
+                <div className="text-[17px] w-auto">Crypto</div>
               </div>
             </button>
           </ul>
@@ -99,14 +67,8 @@ const SettingsHeader = () => {
             role="button"
             className={`py-2 text-[17px] h-[40px] focus:outline-none
     ${
-      mode === "dark"
-        ? "border-primary-A1 text-primary-A2"
-        : "border-primary-A2 text-primary-A1"
-    } 
-    ${
       activeTab === 2 || activeTab === 3 ? "border-t-none border-b-[4px]" : ""
-    }`}
-          >
+    }`}>
             More...
           </div>
           <ul className="shadow menu dropdown-content z-[1] gap-3 bg-white rounded-box w-32">
@@ -114,14 +76,9 @@ const SettingsHeader = () => {
               className={`py-2 text-[17px] h-[40px] focus:outline-none ${
                 activeTab === 2 ? "text-primary-A2" : "text-black"
               }`}
-              onClick={() => handleTabClick(2)}
-            >
+              onClick={() => handleTabClick(2)}>
               <div className="flex flex-col gap-0.5 items-center justify-center px-3">
-                <div
-                  className="text-[17px] w-auto"
-                >
-                  Item 1
-                </div>
+                <div className="text-[17px] w-auto">Item 1</div>
               </div>
             </button>
 
@@ -129,14 +86,9 @@ const SettingsHeader = () => {
               className={`py-2 text-[17px] h-[40px] focus:outline-none ${
                 activeTab === 3 ? "text-primary-A2" : "text-black"
               }`}
-              onClick={() => handleTabClick(3)}
-            >
+              onClick={() => handleTabClick(3)}>
               <div className="flex flex-col gap-0.5 items-center justify-center px-3">
-                <div
-                  className="text-[17px] w-auto"
-                >
-                  Item 2
-                </div>
+                <div className="text-[17px] w-auto">Item 2</div>
               </div>
             </button>
           </ul>
