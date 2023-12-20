a4a161ea994b02f63954094bf80faefc156d866a
upraisertech <akoredesalaudeen54@gmail.com>
2023-12-20 15:49:21 +0100

    updates

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,6 @@
 import { ID, Query } from "appwrite";
 
-import { appwriteConfig, account, databases, storage, avatars, client } from "./config";
+import { appwriteConfig, account, databases, storage, avatars } from "./config";
 import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
 
 // ============================================================
