c4818f321dc9f13b167560837ac0eaa4ea1cf117
upraisertech <akoredesalaudeen54@gmail.com>
2023-12-21 17:56:59 +0100

    updates

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -3,20 +3,36 @@ import { Models } from "appwrite";
 // import { useToast } from "@/components/ui/use-toast";
 import { Loader, PostCard, UserCard } from "@/components/shared";
 import { useGetRecentPosts, useGetUsers } from "@/lib/react-query/queries";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  // const { toast } = useToast();
-
-  const {
-    data: posts,
-    isLoading: isPostLoading,
-    isError: isErrorPosts,
-  } = useGetRecentPosts();
   const {
     data: creators,
     isLoading: isUserLoading,
     isError: isErrorCreators,
   } = useGetUsers(10);
+
+  const {
+    data: postsData,
+    isLoading: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+
+  const [sortedPosts, setSortedPosts] = useState<Models.Document[]>([]);
+
+  useEffect(() => {
+    if (postsData && postsData.documents) {
+      const sorted = postsData.documents
+        .slice()
+        .sort((a: Models.Document, b: Models.Document) => {
+          // Change 'createdAt' to your post creation date property
+          const dateA = new Date(a.createdAt).getTime();
+          const dateB = new Date(b.createdAt).getTime();
+          return dateB - dateA; // Sort in descending order, modify if needed
+        });
+      setSortedPosts(sorted);
+    }
+  }, [postsData]);
 
   if (isErrorPosts || isErrorCreators) {
     return (
@@ -36,11 +52,11 @@ const Home = () => {
       <div className="home-container">
         <div className="home-posts">
           <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
-          {isPostLoading && !posts ? (
+          {isPostLoading && !sortedPosts ? (
             <Loader />
           ) : (
-            <ul className="flex flex-col flex-1 gap-9 w-full ">
-              {posts?.documents.map((post: Models.Document) => (
+            <ul className="flex flex-col flex-1 gap-9 w-full">
+              {sortedPosts?.map((post: Models.Document) => (
                 <li key={post.$id} className="flex justify-center w-full">
                   <PostCard post={post} />
                 </li>
